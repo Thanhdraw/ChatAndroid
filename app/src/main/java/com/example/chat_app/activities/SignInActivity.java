acae768948086ea1f9f59af97dceb8a50a215035
MSI <dangquocthanh2812@gmail.com>
2022-12-11 22:30:34 +0700

    Xong bai(1,2), Xem lai tu luc 19:42s

--- app/src/main/java/com/example/chat_app/activities/SignInActivity.java
+++ app/src/main/java/com/example/chat_app/activities/SignInActivity.java
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.chat_app.R;
 import com.example.chat_app.databinding.ActivitySignInBinding;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -25,6 +29,23 @@ public class SignInActivity extends AppCompatActivity {
     private void setListeners(){
         binding.textCreateNewAccount.setOnClickListener(v ->
                 startActivities(new Intent[]{new Intent(getApplicationContext(), SignUpActivity.class)}));
+        binding.buttonSignIn.setOnClickListener(v -> addDataToFirestore());
+    }
+
+    private void addDataToFirestore(){
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        HashMap<String, Object> data = new HashMap<>();
+        data.put("first_name", "QuocThanh");
+        data.put("last_name", "Dang");
+        database.collection("users")
+                .add(data)
+                .addOnSuccessListener(documentReference -> {
+                    Toast.makeText(getApplicationContext(), "Data Inserted",Toast.LENGTH_SHORT).show();
+                })
+                .addOnFailureListener(exception -> {
+                    Toast.makeText(getApplicationContext(),exception.getMessage(), Toast.LENGTH_SHORT).show();
+                });
+
     }
 
 }
